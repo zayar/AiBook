e158adc7a9b5e5b1437d15fbcf7c17141eb00011
zayarmin <zayarmin@gmail.com>
2025-08-08 16:45:25 +0630

    Remove test file

--- test-controller.js
+++
@@ -1,3 +0,0 @@
-const InvoiceController = require('./src/controllers/invoiceController.ts');
-console.log('Available methods:', Object.getOwnPropertyNames(InvoiceController.default || InvoiceController));
-console.log('Method exists:', typeof (InvoiceController.default || InvoiceController).createOrRefreshShareLink);
